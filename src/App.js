c665321d014bfcde2a29d436c6fcc43ec39990dd
Rish121296 <88335034+Rish121296@users.noreply.github.com>
2023-02-15 15:21:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload WELCOME TO WEBSERVER.
+          Edit <code>src/App.js</code> and save to reload WELCOME TO DISCERN'S WEBSERVER.
         </p>
         <a
           className="App-link"
